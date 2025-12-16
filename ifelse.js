2c062636ad5bf819205aef684de35736ae932f74
Sarvesh <apshete2003@gmail.com>
2025-12-16 10:29:45 +0530

    test

---
+++ ifelse.js
@@ -0,0 +1,62 @@
+// operators - <, >, <=, >=, !, ==, !=, ===(triple equals also checks the datatypes), !==
+
+//if- else statement
+
+// const temp = 35;
+
+// if(temp >= 40) {
+//     console.log("It is a hot day");
+// }
+// else{
+//     console.log("It is a hotter day");
+// }
+
+// Nested if-else statement using else-if
+
+// const balance = 1000;
+
+// if(balance < 500){
+//     console.log("Balance less than 500");
+// }
+// else if(balance < 750){
+//     console.log("Balance less than 750");
+// }
+// else{
+//     console.log("Balance less than 1200");
+// }
+
+// and , or operator
+
+// const balance = 1000;
+// const age = 50;
+
+// if(balance > 750 && age > 40) {
+//     console.log("True");
+// } // using '&&'(and) operator
+// if(balance > 1000 || age > 45) {
+//     console.log("True"); // using'||'(or) operator
+// }
+
+
+// nullish coalescing operator '??' 
+// nullish coalescing operator and terniary operator are different
+// The ?? (nullish coalescing) operator gives a default value only when the left side is null or undefined
+
+// let val1;
+// val1 = 10 ?? 10 // here as there is no null/undefined so 1st value will come
+// val1 = null ?? 10 // here as there is null so next value will come
+// val1 = undefined ?? 10 // here as there is undefined so next value will come
+// val1 = undefined ?? 10 ?? 20 // here as there is undefined next value will come among all available values
+
+// console.log(val1);
+
+// terniary operator
+
+// condition ? true : false
+
+// const price = 100;
+
+// price > 80 ? console.log("greater than 80") : console.log("less than 80");
+ 
+
+
